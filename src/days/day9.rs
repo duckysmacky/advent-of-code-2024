23b8e963663f0363d746f547e237bef4150d900b
DuckySmacky <kriperoka@gmail.com>
2024-12-13 00:16:30 +0300

    Progress on day 9 part 2

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -1,14 +1,14 @@
 use crate::utils;
 
 pub fn run() {
-    println!("Part 1: {}", part1());
+    // println!("Part 1: {}", part1());
     println!("Part 2: {}", part2());
 }
 
 fn part1() -> usize {
     let disk_map = get_disk_map(1);
     let mut file_i = 0;
-    let mut block_map = disk_map.chunks(2)
+    let mut map = disk_map.chunks(2)
         .map(|pair| {
             let (file_c, space_c) = (pair[0], *pair.get(1).unwrap_or(&0));
             let files = (0..file_c).map(|_| file_i).collect::<Vec<i32>>();
@@ -19,11 +19,11 @@ fn part1() -> usize {
         .flatten()
         .collect::<Vec<i32>>();
     
-    println!("Block map: {:?}", block_map);
+    println!("Block map: {:?}", map);
 
     let mut last_i = 0;
-    for j in (0..block_map.len()).rev() {
-        if block_map[j] == -1 {
+    for j in (0..map.len()).rev() {
+        if map[j] == -1 {
             continue;
         }
 
@@ -31,8 +31,8 @@ fn part1() -> usize {
             break;
         }
 
-        for i in last_i..block_map.len() {
-            if block_map[i] != -1 {
+        for i in last_i..map.len() {
+            if map[i] != -1 {
                 continue;
             }
 
@@ -40,29 +40,117 @@ fn part1() -> usize {
                 break;
             }
 
-            block_map[i] = block_map[j];
-            block_map[j] = -1;
+            map[i] = map[j];
+            map[j] = -1;
             last_i = i;
-            /* println!("{} {}\n{:?}\n", j, i, block_map.iter()
-                .map(|x| x.to_string())
-                .map(|x| if x == "-1" {
-                        ".".to_string()
-                    } else {
-                        x
-                })
-                .collect::<String>()); */
             break;
         }
     }
 
-    block_map.into_iter()
+    map.into_iter()
         .enumerate()
         .map(|(i, x)| i * if x != -1 { x as usize } else { 0 })
         .sum()
 }
 
 fn part2() -> usize {
-    0
+    let disk_map = get_disk_map(0);
+    let mut file_i = 0;
+    let mut map = disk_map.chunks(2)
+        .map(|pair| {
+            let (file_c, space_c) = (pair[0], *pair.get(1).unwrap_or(&0));
+            let files = (0..file_c).map(|_| file_i).collect::<Vec<i32>>();
+            let spaces = (0..space_c).map(|_| -1).collect::<Vec<i32>>();
+            file_i += 1;
+            [files, spaces].into_iter().flatten().collect::<Vec<i32>>()
+        })
+        .flatten()
+        .collect::<Vec<i32>>();
+    
+    println!("Block map: {:?}", map);
+
+    let mut spaces: Vec<(usize, usize)> = vec![];
+    let mut space_start = 0;
+    let mut space_len = 0;
+    for i in 0..map.len() {
+        if map[i] != -1 {
+            if space_start != 0 {
+                spaces.push((space_start, space_len));
+            }
+
+            space_start = 0;
+            space_len = 0;
+            continue;
+        }
+
+        if space_start == 0 {
+            space_start = i;
+        } 
+
+        space_len += 1;
+    }
+
+    println!("Spaces: {:?}", spaces);
+
+    let mut files: Vec<(usize, usize)> = vec![];
+    let mut file_index = -1;
+    let mut file_len = 0;
+    for j in (0..map.len()).rev() {
+        if map[j] != file_index || map[j] == -1 {
+            if file_index != -1 {
+                files.push((file_index.try_into().unwrap(), file_len));
+                file_index = -1;
+                file_len = 0;
+            }
+        }
+
+        if file_index == -1 && map[j] != -1 {
+            file_index = map[j];
+            file_len = 0;
+        }
+
+        file_len += 1;
+    }
+
+    println!("Files: {:?}", files);
+
+    for i in 0..files.len() {
+        let (file_number, file_len) = files[i];
+
+        let mut j = 0;
+        while j < spaces.len() {
+            let (space_start, space_len) = spaces[j];
+
+            if space_len >= file_len {
+                for offset in 0..file_len {
+                    map[space_start + offset] = file_number as i32;
+                    map[i - offset] = -1;
+                }
+
+                if space_len > file_len {
+                    spaces[j] = (space_start + file_len, space_len - file_len);
+                } else {
+                    spaces.remove(j);
+                }
+            }
+
+            j += 1;
+        }
+
+        println!("{} {}\n{:?}\n", j, i, map.iter()
+            .map(|x| x.to_string())
+            .map(|x| if x == "-1" {
+                    ".".to_string()
+                } else {
+                    x
+            })
+            .collect::<String>());
+    }
+
+    map.into_iter()
+        .enumerate()
+        .map(|(i, x)| i * if x != -1 { x as usize } else { 0 })
+        .sum()
 }
 
 fn get_disk_map(part: u8) -> Vec<u8> {
